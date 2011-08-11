2d38ad5736411aa794e699ea388f045b9090df84
fernandolopez <fernando.e.lopez@gmail.com>
2011-08-11 16:33:41 -0300

    Actualizaciones de estilo.

--- js/ext.JQueryLihuen.js
+++ js/ext.JQueryLihuen.js
@@ -2,7 +2,12 @@
 // The next operations remove attributes ment to browsers without javascript support
 	$(".extJQueryLihuenTabsList").removeAttr("style");
 	$(".extJQueryLihuenTab").removeAttr("style");
+// In Lihuen wiki we want to remove some styles
+	$(".extJQueryLihuenTab > div").removeAttr("style");
 // Creation of the widgets
-	$(".extJQueryLihuenTabs").tabs({event:"mouseover"});
+//	$(".extJQueryLihuenTabs").tabs({event:"mouseover"});
+	$(".extJQueryLihuenTabs").tabs();
+	$(".ui-tabs .ui-tabs-panel").css("padding", "0.5em");
+	$(".ui-widget-content a").css("color", $("a").css("color"));
 })();
 
