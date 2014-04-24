3a1f57cac22bfd1792f754cbb8ef87e19de1ec5e
lihuen <lihuen@proyectos.linti.unlp.edu.ar>
2014-04-24 16:59:18 -0300

    Carrusel o slideshow de imágenes con JCarousel agregado

--- js/ext.JQueryLihuen.js
+++ js/ext.JQueryLihuen.js
@@ -1,13 +1,41 @@
-(function (){
-// The next operations remove attributes ment to browsers without javascript support
+$(function(){
+	// The next operations remove attributes ment to browsers without javascript support
 	$(".extJQueryLihuenTabsList").removeAttr("style");
 	$(".extJQueryLihuenTab").removeAttr("style");
-// In Lihuen wiki we want to remove some styles
+
+	// In Lihuen wiki we want to remove some styles
 	$(".extJQueryLihuenTab > div").removeAttr("style");
-// Creation of the widgets
-//	$(".extJQueryLihuenTabs").tabs({event:"mouseover"});
+
+	// Creation of the widgets
+	// $(".extJQueryLihuenTabs").tabs({event:"mouseover"});
 	$(".extJQueryLihuenTabs").tabs();
 	$(".ui-tabs .ui-tabs-panel").css("padding", "0.5em");
 	$(".ui-widget-content a").css("color", $("a").css("color"));
-})();
 
+	// ------- JCarousel --------
+	var jcarousel = $('.jcarousel');
+	jcarousel.jcarousel({wrap: 'both', vertical: false});
+	// Paginación
+	jcarousel.append('<div class="jcarousel-pagination"></div>');
+	$('.jcarousel-pagination')
+		.jcarouselPagination({
+			item: function(page) {
+				return '<a href="#' + page + '">' + page + '</a>';
+			},
+			perPage: 1
+		})
+		.on('jcarouselpagination:active', 'a', function() {
+			$(this).addClass('active');
+		})
+		.on('jcarouselpagination:inactive', 'a', function() {
+			$(this).removeClass('active');
+		});
+	// Controles
+	jcarousel.append('<a href="#" class="jcarousel-prev">&lt;</a>');
+	jcarousel.append('<a href="#" class="jcarousel-next">&gt;</a>');
+	$('.jcarousel-prev').jcarouselControl({target: '-=1'});
+	$('.jcarousel-next').jcarouselControl({target: '+=1'});
+	// Scroll automático
+	jcarousel.jcarouselAutoscroll();
+	// ------- end JCarousel --------
+});
